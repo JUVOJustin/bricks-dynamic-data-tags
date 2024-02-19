376fe9c1c8c6a08904431714bfe36ccf93b2ca8d
Justin Vogt <mail@justin-vogt.de>
2024-02-19 13:17:35 +0100

    Update DDT_Registry.php
    Initialise later

--- src/DDT_Registry.php
+++ src/DDT_Registry.php
@@ -9,7 +9,7 @@ class DDT_Registry
     private array $storage = [];
 
     private function __construct() {
-        add_action('init', function() {
+        add_action('wp', function() {
             $this->registerDataTags();
         }, 99);
     }
